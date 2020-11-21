b3e7a68392474276d2350cdc319cd84262ffc4bf
Daniel N. Hansten <daniel.hansten@bouvet.no>
2020-11-21 21:55:39 +0100

    BASE REPO

---
+++ src/main.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+
+/* 1000 msec = 1 sec */
+
+
+/* The devicetree node identifier for the "led0" alias. */
+
+
+
+
+/* A build error here means your board isn't set up to blink an LED. */
+
+
+
+
+
+void main(void)
+{
+
+printk("hello");
+
+}
