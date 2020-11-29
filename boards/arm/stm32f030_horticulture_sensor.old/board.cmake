04d9054cd49be76afcc331e98eb785c7eb447357
daniel-nicolai-hansten <dnh2000@gmail.com>
2020-11-29 13:55:13 +0100

    Bygger nesten med ny boardfil

--- boards/arm/stm32f030_horticulture_sensor.old/board.cmake
+++ boards/arm/stm32f030_horticulture_sensor.old/board.cmake
@@ -4,8 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-board_runner_args(stm32flash "--start-addr=0x08000000")
-
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/stm32flash.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
